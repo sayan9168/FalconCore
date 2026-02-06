7d0ca448ec0e5393cc09ee844c8302f3ac1a2289
Sayan the researcher <sm6881164@gmail.com>
2026-02-06 11:12:55 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
-// src/main.rs - FalconCore Runtime Entry Point
+use falconcore::lexer::{Lexer, TokenType};
+
 fn main() {
-    println!("FalconCore v0.1 - Native Runtime Starting...");
-    println!("This is the beginning of Falcon's own VM.");
-    println!("Lexer, Parser, Bytecode Compiler & VM coming soon!");
-    println!("Built by SAYAN - Let's make FalconCore real!");
+    println!("FalconCore v0.1 - Lexer Test");
+
+    let code = r#"
+    secure let x = 42
+    print "Hello from FalconCore!"
+    if x > 10 {
+        print "x is greater than 10"
+    }
+    "#;
+
+    let mut lexer = Lexer::new(code);
+    loop {
+        let token = lexer.next_token();
+        println!("{:?}", token);
+        if token.kind == TokenType::Eof {
+            break;
+        }
+    }
+
+    println!("Lexer test complete!");
 }
