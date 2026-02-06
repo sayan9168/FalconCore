d95a03832bb12388166305183f9cd18642a5821d
Sayan the researcher <sm6881164@gmail.com>
2026-02-06 11:23:02 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -50,3 +50,35 @@ fn main() {
 
     println!("Parsing complete!");
 }
+mod lexer;
+mod parser;
+
+use lexer::Lexer;
+use parser::Parser;
+
+fn main() {
+    println!("FalconCore v0.1 - Lexer + Parser Test");
+
+    let code = r#"
+        secure let x = 42
+        secure const pi = 3.14
+        print "Hello from FalconCore!"
+        if x > 10 {
+            print "x is greater than 10"
+        } else {
+            print "x is small"
+        }
+    "#;
+
+    let lexer = Lexer::new(code);
+    let mut parser = Parser::new(lexer);
+
+    let ast = parser.parse();
+
+    println!("AST:");
+    for node in ast {
+        println!("{:#?}", node);
+    }
+
+    println!("Parsing complete!");
+}
