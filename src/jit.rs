de60de233804003c6045cea33eace6a3157537eb
Sayan the researcher <sm6881164@gmail.com>
2026-02-24 10:22:49 +0530

    Update jit.rs

--- src/jit.rs
+++ src/jit.rs
@@ -13,3 +13,61 @@ pub fn jit_compile(code: &Vec<Opcode>) -> Result<(), String> {
 
     Ok(())
   }
+// src/jit.rs - FalconCore JIT Compiler (Cranelift JIT backend)
+use cranelift::codegen::ir::{types, AbiParam, Function, UserFuncName};
+use cranelift::codegen::Context;
+use cranelift_jit::{JITBuilder, JITModule};
+use cranelift_module::Module;
+
+pub fn jit_test_loop() -> Result<(), String> {
+    let mut builder = JITBuilder::new(cranelift_module::default_libcall_names());
+    let mut module = JITModule::new(builder);
+    let mut ctx = module.make_context();
+
+    let mut sig = module.make_signature();
+    sig.returns.push(AbiParam::new(types::I32));
+
+    let mut func = Function::with_name_signature(UserFuncName::user(0, 0), sig);
+    let mut builder_ctx = FunctionBuilderContext::new();
+    let mut builder = FunctionBuilder::new(&mut func, &mut ctx.func, &mut builder_ctx);
+
+    let entry = builder.create_block();
+    builder.append_block_params_for_function_params(entry);
+    builder.switch_to_block(entry);
+    builder.seal_block(entry);
+
+    let mut counter = builder.ins().iconst(types::I64, 0);
+    let target = builder.ins().iconst(types::I64, 10000);
+
+    let loop_header = builder.create_block();
+    let loop_body = builder.create_block();
+    let loop_exit = builder.create_block();
+
+    builder.ins().jump(loop_header, &[]);
+    builder.switch_to_block(loop_header);
+
+    let cond = builder.ins().ifcmp(counter, target, CondCode::Lt);
+    builder.ins().brnz(cond, loop_body, &[]);
+    builder.ins().jump(loop_exit, &[]);
+
+    builder.switch_to_block(loop_body);
+    counter = builder.ins().iadd_imm(counter, 1);
+    builder.ins().jump(loop_header, &[counter]);
+
+    builder.switch_to_block(loop_exit);
+    builder.ins().return_(&[counter]);
+
+    builder.finalize();
+
+    let id = module.declare_function("loop_test", Linkage::Export, &ctx.func.signature).unwrap();
+    module.define_function(id, &ctx).unwrap();
+
+    let code = module.finish();
+    let (func_ptr, _) = code.get_function(id).unwrap();
+
+    let func = unsafe { std::mem::transmute::<*const u8, extern "C" fn() -> i64>(func_ptr) };
+    let result = func();
+    println!("JIT loop 10000 times result: {}", result);
+
+    Ok(())
+                                   }
